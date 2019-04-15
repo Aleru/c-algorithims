84c7ed378e435483cb7f6d3d13c5e9a5f1c79bc8
Aleru <ryuppal@gmail.com>
2019-04-15 16:56:09 -0400

    Add existing file

---
+++ algo.c
@@ -0,0 +1,18 @@
+// create a structure to hold an array and a number 
+// create function struct{array,num} sort_and_count_inv(array)
+// when length of the array 0 or 1 return struct{starting_array, 0}
+//recursively solve first half
+// recursively solve second half 
+//call a function to merge the two sorted arrays and count split inversions
+// 
+
+
+
+
+
+#include<stdio.h>
+
+int main(void) {
+    printf("Hello World\n");
+    return 0;
+}
